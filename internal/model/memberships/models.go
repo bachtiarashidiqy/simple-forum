0186dc4bf79842d287077b974b5f8bfe79082664
bachtiarashidiqy <bachtiar.asyidiqy@gmail.com>
2025-01-02 21:28:15 +0700

    feat(models): add User model and Sign Up request

---
+++ internal/model/memberships/models.go
@@ -0,0 +1,28 @@
+package memberships
+
+import "time"
+
+type (
+	SingUpRequest struct {
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	LoginRequest struct {
+		Login    string `json:"login"`
+		Password string `json:"password"`
+	}
+)
+
+type (
+	UserModel struct {
+		ID        int64     `db:"id"`
+		Username  string    `db:"username"`
+		Email     string    `db:"email"`
+		Password  string    `db:"password"`
+		CreatedAt time.Time `db:"created_at"`
+		UpdatedAt time.Time `db:"updated_at"`
+		CreatedBy string    `db:"created_by"`
+		UpdatedBy string    `db:"updated_by"`
+	}
+)
