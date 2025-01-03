061fda08a5337fdcafd5078de44f07243c9fecbe
bachtiarashidiqy <bachtiar.asyidiqy@gmail.com>
2025-01-03 21:21:56 +0700

    feat: initiate repository and service in main package

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"github.com/bachtiarashidiqy/simple-forum/internal/configs"
+	membershipRepo "github.com/bachtiarashidiqy/simple-forum/internal/repository/memberships"
+	"github.com/bachtiarashidiqy/simple-forum/pkg/internalsql"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	r := gin.Default()
+	var (
+		cfg *configs.Config
+	)
+	err := configs.Init(
+		configs.WithConfigFolder(
+			[]string{"./internal/configs/"}),
+		configs.WithConfigFile("config"),
+		configs.WithConfigType("yaml"))
+	if err != nil {
+		log.Fatal("error loading config", err)
+	}
+	cfg = configs.Get()
+	db, err := internalsql.Connect(cfg.Database.DataSourceName)
+	if err != nil {
+		log.Fatal("error connecting to database", err)
+	}
+	_ := membershipRepo.NewRepository(db)
+	err = r.Run(cfg.Service.Port)
+	if err != nil {
+		return
+	}
+}
